eabb0aebb0601a80637a3aca6c9799e260615549
Vy Tran <71632559+vytran2001@users.noreply.github.com>
2023-02-10 14:14:03 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import TrafficLight from "./TrafficLight/TrafficLight";
+
+function App() {
+  return (
+    <div className="App">
+      <TrafficLight />
+    </div>
+  );
+}
+
+export default App;
